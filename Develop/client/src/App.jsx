59627eecfc87bb833d8ce0c17935bf841334b6ef
kaitlynmooney <kmooney806@gmail.com>
2024-08-11 22:01:50 -0400

    added files

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,31 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import "./App.css";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
-import Navbar from './components/Navbar';
+const client = new ApolloClient({
+  // request: (operation) => {
+  //   const token = localStorage.getItem("id_token");
+
+  //   operation.setContext({
+  //     headers: {
+  //       authorization: token ? `Bearer ${token}` : "",
+  //     },
+  //   });
+  // },
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+      </>
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# hw-21-book-se
+# Book Search Engine
+
+## Description
+
+The purpose of this project was to take a search engine application built with a RESTful API and refactor it to be a GraphQL API built with Apollo Server. The app was built using the MERN stack, with a React front end, MongoDB database, and Node.js/Express.js server and API.
+
+## Usage
+
+To use this application sign up or login and search and save your favorite books!
+
+[Link to Deployed App](https://hw-21-book-se.onrender.com)
+
+## Credits
+
+Source code courtesy of bootcamp.
